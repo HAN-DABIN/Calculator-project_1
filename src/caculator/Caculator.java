94b58c7e2c4b673053b46b3e8c47ea60fcd3fb51
HAN-DABON <sinmint11@gmail.com>
2026-03-24 16:39:28 +0900

    step 1 주석

--- src/caculator/Caculator.java
+++ src/caculator/Caculator.java
@@ -1,4 +1,20 @@
 package caculator;
 
 public class Caculator {
+
+    public int calcutate (int num1, int num2, char operator) {
+        int result = 0;
+
+        if (operator == '+') {
+            result = num1 + num2;
+        } else if (operator == '-') {
+            result = num1 - num2;
+        } else if (operator == '*') {
+            result = num1 * num2;
+        } else if (operator == '/') {
+            result = num1 / num2;
+        }
+        return result;
+    }
+
 }
