29c3c74b35e7c983a67b955102cbfc44194fd151
HAN-DABON <sinmint11@gmail.com>
2026-03-25 12:54:05 +0900

    step 2 v2

--- src/calculatorStep2/App2.java
+++ src/calculatorStep2/App2.java
@@ -1,5 +1,6 @@
 package calculatorStep2;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class App2 {
@@ -10,13 +11,12 @@ public class App2 {
         // calcultator 클래스 객체 생성
         Calculator2 calculator = new Calculator2();
 
-        // 전체 메뉴 반복문
+        System.out.println("계산기를 작동합니다.");
         while (true) {
-            System.out.println("=== 계산기를 작동합니다 ===");
             System.out.println("=== 메뉴 ===");
-            System.out.println("=== 1. 계산하기 ===");
-            System.out.println("=== 2. 계산 기록 보기 ===");
-            System.out.println("=== 0. 종료 ===");
+            System.out.println("1. 계산하기");
+            System.out.println("2. 연산 기록");
+            System.out.println("0. 종료");
             System.out.println("원하시는 메뉴를 숫자로 입력해주세요.");
 
             // 메뉴 입력받기
@@ -26,7 +26,7 @@ public class App2 {
             // 0을 고르면 계산기 종료
             if (select == 0) {
                 System.out.println("계산기를 종료합니다.");
-                break;
+                System.exit(0);
             }
 
             if (select == 1) {
@@ -63,7 +63,6 @@ public class App2 {
                             System.out.println("지원하지 않는 연산자입니다.");
                         }
                     }
-
                     int num2;
                     // 두 번째 숫자 입력 반복문
                     while (true) {
@@ -88,22 +87,54 @@ public class App2 {
 
                     System.out.println("결과: " + num1 + operator + num2 + " = " + result);
 
-
-                    System.out.println("계속 계산하시겠습니까? (yes - 계속 계산 / exit - 종료)");
+                    System.out.println("계속 계산하시겠습니까? (yes - 계속 계산 / no - 메뉴로 돌아가기)");
                     // 입력 받기
                     String exit = scanner.nextLine();
                     // exit 입력 시 프로그램 종료
-                    if (exit.equals("exit")) {
-                        System.exit(0);
+                    if (exit.equals("no")) {
+                       break;
                         // yes 입력 시 문구 출력 후 while문 처음으로 돌아감
                     } else if (exit.equals("yes")) {
                         System.out.println("다시 계산합니다.");
                     }
                 }
             }
+            if (select == 2) {
+                System.out.println("연산 기록을 확인합니다.");
+                System.out.println("1. 연산 기록 조회");
+                System.out.println("2. 연산 기록 삭제");
+
+                int select2 = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (select2) {
+                    case 1:
+                        // getter 활용
+                        List<Integer> newCal = calculator.getResult();
+                        System.out.println(newCal);
+                        break;
+                    case 2:
+                        // setter 활용
+                        System.out.println("첫 번째 계산 기록을 삭제합니다");
+                            calculator.removeResult();
+                            break;
+                        }
+
+                System.out.println("메뉴로 돌아가시겠습니까? (yes - 메뉴로 돌아가기 / exit - 프로그램 종료)");
+                // 입력 받기
+                String exit2 = scanner.nextLine();
+                // yes 입력 시 문구 출력 후 while문(메뉴반복) 처음으로 돌아감
+                if (exit2.equals("yes")) {
+                    // exit 입력 시 프로그램 종료
+                } else if (exit2.equals("exit")) {
+                    System.out.println("프로그램을 종료합니다.");
+                    System.exit(0);
+                }
+                }
+            }
         }
-    }
 }
+
 
 
 
--- src/calculatorStep2/Calculator2.java
+++ src/calculatorStep2/Calculator2.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Calculator2 {
 
     // 연산 결과를 저장하는 컬렉션 타입 필드를 외부에서 직접 접근 하지 못하도록 수정 - 캡슐화
-    private List<Integer> results = new ArrayList<>();
+    private ArrayList<Integer> resultList = new ArrayList<>();
 
     // 연산 수행 메서드 생성
     public int calculate(int num1, int num2, char operator) {
@@ -22,18 +22,26 @@ public class Calculator2 {
         } else if (operator == '/') {
             result = num1 / num2;
         }
-        // 연산 결과 저장
-        history(result);
-        // 반환값
+        resultList.add(result);
         return result;
     }
-    // 세터 - 연산 결과 추가
-    private void history(int result) {
-        results.add(result);
+    // getter
+    public List<Integer> getResult() {
+    return this.resultList;
     }
+    // setter
 
-    public List<Integer> getResults() {
-        return new ArrayList<>(results);
+    // 첫번째 기록 삭제
+    public void removeResult() {
+        if (!resultList.isEmpty()) {
+            resultList.remove(0);
+            System.out.println("현재 저장된 결과는 " + resultList + "입니다.");
+        } else {
+            System.out.println("삭제할 결과가 없습니다.");
+        }
     }
-
 }
+
+
+
+
